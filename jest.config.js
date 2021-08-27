b53c2bb204f8ece7c8001c3d8f1f97645032a192
Crístofer Domingues <cristofer.domingues@gmail.com>
2021-08-27 10:30:59 -0300

    resposive

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const merge = require('lodash.merge')
+const config = require('kcd-scripts/jest')
+
+module.exports = merge(config, {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'vue'],
+  moduleNameMapper: {
+   // '@testing-library/vue': '<rootDir>/src/test.config/index.js',
+  },
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/__tests__/**'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules',
+  ],
+  transformIgnorePatterns: [
+
+  ]
+})
