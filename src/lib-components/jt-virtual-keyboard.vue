7e11823775c47dbe40f9523d11ade12285470391
Crístofer Domingues <cristofer.domingues@gmail.com>
2021-08-30 10:27:48 -0300

    build 1.1.2

--- src/lib-components/jt-virtual-keyboard.vue
+++ src/lib-components/jt-virtual-keyboard.vue
@@ -47,5 +47,105 @@ export default /*#__PURE__*/ defineComponent({
 </template>
 
 <style>
+.simple-keyboard {
+  top: 100%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 100;
+  transition: all 0.3s ease-out;
+}
+.simple-keyboard.hg-theme-default {
+  background-color: #dce8f2;
+}
 
+.simple-keyboard.hg-theme-default .hg-rows .hg-row .hg-button > span {
+  color: #092147;
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.simple-keyboard.numeric-theme .hg-rows .hg-row .hg-button > span {
+  font-size: 30px;
+}
+
+.simple-keyboard.hg-theme-default .hg-candidate-box .hg-candidate-box-prev,
+.simple-keyboard.hg-theme-default .hg-candidate-box .hg-candidate-box-next {
+  background: #dce8f2;
+  color: #092147;
+}
+
+.simple-keyboard.hg-theme-default .hg-candidate-box .hg-candidate-box-list {
+  background-color: #fff;
+}
+
+.simple-keyboard.show {
+  top: calc(100% - 230px);
+}
+
+.simple-keyboard.hide {
+  top: 100%;
+}
+.simple-keyboard.hg-theme-default .hg-button.hg-button-close {
+  flex: 0.11;
+}
+
+.simple-keyboard.hg-theme-default .hg-button.hg-button-close:only-child {
+  flex: 1;
+}
+
+.simple-keyboard.numeric-theme .hg-row:not(:last-child) {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(3, calc(100% / 3));
+}
+
+.simple-keyboard.numeric-theme .hg-button.hg-standardBtn {
+  width: auto;
+}
+
+/* Small Devices */
+@media (min-width: 43em) {
+  .simple-keyboard.hg-theme-default .hg-rows .hg-row .hg-button > span {
+    font-size: 15px !important;
+  }
+
+  .simple-keyboard.numeric-theme .hg-rows .hg-row .hg-button > span {
+    font-size: 20px !important;
+  }
+}
+
+/* Large Devices */
+@media (min-width: 62em) {
+  .simple-keyboard.hg-theme-default .hg-rows .hg-row .hg-button > span {
+    font-size: 20px !important;
+  }
+
+  .simple-keyboard.numeric-theme .hg-rows .hg-row .hg-button > span {
+    font-size: 25px !important;
+  }
+}
+
+/* Extra Large Devices */
+@media (min-width: 82em) {
+  .simple-keyboard.hg-theme-default .hg-rows .hg-row .hg-button > span {
+    color: #092147;
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  .simple-keyboard.numeric-theme .hg-rows .hg-row .hg-button > span {
+    font-size: 30px;
+  }
+}
+
+@media (max-width: 62em) {
+  .simple-keyboard.hg-theme-default .hg-rows .hg-row .hg-button > span {
+    font-size: 10px !important;
+  }
+
+  .simple-keyboard.numeric-theme .hg-rows .hg-row .hg-button > span {
+    font-size: 15px !important;
+  }
+}
 </style>
